350ee01bfdb40b32f43cd0f498662dc484cfd236
ravertass <ravertass@gmail.com>
2013-07-11 14:01:16 +0200

    Pillade med input-grejerna
    För tillfället fungerar saker inte riktigt som det ska och allt är inte
    så genomtänkt.

--- src/model/Model.java
+++ src/model/Model.java
@@ -11,12 +11,48 @@ package model;
 public class Model {
 
 	public CursorM cursor;
+	private int mouseX, mouseY;
+	private boolean leftClick, rightClick;
 	
 	public Model() {
 		cursor = new CursorM();
 	}
 	
+	/**
+	 * This method will be run for every iteration of the main game loop.
+	 */
 	public void update() {
-		cursor.update();
+		cursor.update(mouseX, mouseY);
+		/**if leftClick:
+			for grejer in arraylist:
+				if man har klickat på grejen:
+					grej.clicked();**/
+		// reset mouse clicks
+		leftClick = false;
+		rightClick = false;
+	}
+
+	public void setMouseX(int x) {
+		mouseX = x;
+	}
+
+	public void setMouseY(int y) {
+		mouseY = y;
+	}
+	
+	/**
+	 * This method will be run if the left mouse button is clicked, to let
+	 * the model know that it's so.
+	 */
+	public void leftClick() {
+		leftClick = true;
+	}
+
+	/**
+	 * This method will be run if the right mouse button is clicked, to let
+	 * the model know that it's so.
+	 */
+	public void rightClick() {
+		rightClick = true;
 	}
 }
